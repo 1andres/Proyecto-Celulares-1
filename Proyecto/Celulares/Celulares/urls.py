b08ad50d751991f8a1e84b0d2393d8e06dc51b3d
andres ortiz <ortizandres94@misena.edu.co>
2015-04-15 19:26:04 -0500

    commit2
    Publicacion proyecto

---
+++ Proyecto/Celulares/Celulares/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+import settings
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Celulares.views.home', name='home'),
+    # url(r'^Celulares/', include('Celulares.foo.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^',include('Celulares.apps.home.urls')),
+    url(r'^',include('Celulares.apps.ventas.urls')),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
+    url(r'^',include('Celulares.apps.webservices.ws_productos.urls')),
+)
